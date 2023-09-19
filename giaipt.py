10fa09d8b80501c5e844feff976dace411ed3337
Nguyen Huu Toan <nguyenhuutoanhh@gmail.com>
2023-09-19 15:32:00 +0700

    sua

--- giaipt.py
+++ giaipt.py
@@ -3,18 +3,18 @@ import numpy as np
 n=int(input("Nhap n: "))
 a=np.zeros((n,n))
 b=np.zeros((n))
+while n>1:
+    for i in range(n):
+        for j in range(n):
+            a[i,j]=float(input(f"a{i+1},{j+1}: "))
+    for i in range(n):
+        b[i] = float(input(f"b{i + 1}: "))
 
-for i in range(n):
-    for j in range(n):
-        a[i,j]=float(input(f"a{i+1},{j+1}: "))
-for i in range(n):
-    b[i] = float(input(f"b{i + 1}: "))
-
-det= np.linalg.det(a)
-if det==0:
-    print("pt vo nghiem")
-else:
-    x=np.linalg.solve(a,b)
-    print(x)
+    det= np.linalg.det(a)
+    if det==0:
+        print("pt vo nghiem")
+    else:
+        x=np.linalg.solve(a,b)
+        print(x)
 
 
